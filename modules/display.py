84b8f9c95c8f83b0a47368d2e5049eded57cde0d
Tom <tkelross@gmail.com>
2017-10-23 17:13:31 +0100

    Disaply.py now formatted using spaces instead of tabs

--- modules/display.py
+++ modules/display.py
@@ -2,55 +2,54 @@ import os
 import sys
 import time
 
+
 class DisplayManager():
+    _line_nums = 40
+    _print_func = print
+    input_func = input
 
-	_line_nums = 40
-	_print_func = print
-	_input_func = input
+    def __init__(self):
+        # Fill the display with empty lines
+        self.lines = ['' for x in range(self._line_nums)]
+        self.top_bar = "Ripper v1.0"
 
-	def __init__(self):
-		#Fill the display with empty lines
-		self.lines = ['' for x in range(self._line_nums)]
-		self.top_bar = "Ripper v1.0"
+    def print(self, message=""):
+        # Add
+        self.lines.pop(0)
+        self.lines.append(message)
+        self.update_display()
 
-	def print(self,message=""):
-		#Add 
-		self.lines.pop(0)
-		self.lines.append(message) 
-		self.update_display()
+    # https://stackoverflow.com/questions/4099422/printing-slowly-simulate-typing
+    def type(self, message, speed=0.03):
+        self.lines.pop(0)
+        self.lines.append('')
+        for char in message:
+            self.lines[-1] = self.lines[-1] + char
+            time.sleep(speed)
+            self.update_display()
 
-	#https://stackoverflow.com/questions/4099422/printing-slowly-simulate-typing
-	def type(self,message,speed=0.03):
-		self.lines.pop(0)
-		self.lines.append('') 
-		for char in message:
-		    self.lines[-1] = self.lines[-1] + char
-		    time.sleep(speed)
-		    self.update_display()
+    def update_top_bar(self, message):
+        self.top_bar = message
 
-	def update_top_bar(self,message):
-		self.top_bar = message
+    def clear_display(self):
+        # https://stackoverflow.com/questions/2084508/clear-terminal-in-python
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-	def clear_display(self):
-		#https://stackoverflow.com/questions/2084508/clear-terminal-in-python
-		os.system('cls' if os.name == 'nt' else 'clear')
+    def reset_display(self):
+        self.lines = ['' for x in range(self._line_nums)]
 
-	def reset_display(self):
-		self.lines = ['' for x in range(self._line_nums)]
+    def show_display(self):
+        self._print_func(self.top_bar)
+        for line in self.lines:
+            self._print_func(line)
 
-	def show_display(self):
-		self._print_func(self.top_bar)
-		for line in self.lines:
-			self._print_func(line)
+    def update_display(self):
+        self.clear_display()
+        self.show_display()
 
-	def update_display(self):
-		self.clear_display()
-		self.show_display()
-
-	def get_input(self,message):
-		self.update_display()
-		return self._input_func(message)
-
+    def get_input(self, message):
+        self.update_display()
+        return self._input_func(message)
 
 # disp = DisplayManager()
 # print = disp.print
@@ -61,5 +60,3 @@ class DisplayManager():
 # 	disp.type("Welcome to Ripper")
 # 	disp.type("Can you solve the mystery",0.08)
 # 	print(input(">"))
-	
-
