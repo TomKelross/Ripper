4d46799be573a246961b0613bedd5fbf7acc8fce
Tom <tkelross@gmail.com>
2017-10-24 15:13:15 +0100

    put colorama library directly in repo as people were having problems with pip

--- events.py
+++ events.py
@@ -3,8 +3,8 @@ def murder_one(context):
     context['display'].print('Someone was murdered in the night')
 
 def start_of_game(context):
-    context["display"].type("Welcome to Ripper. You are detective John S")
-    context["display"].type("Report to the police sargeant")
+    context["display"].type("Welcome to Ripper. You are detective John S",0.01)
+    context["display"].type("Report to the police sargeant",0.05)
 
 
 def add_events(narrative):
