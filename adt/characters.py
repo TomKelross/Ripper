d17789cb7441ef2f88473ac875035f2d67bfab53
PeterGhawi <22836215+PeterGhawi@users.noreply.github.com>
2017-10-25 16:44:56 +0100

    Added all characters

--- adt/characters.py
+++ adt/characters.py
@@ -1,3 +1,166 @@
+diane_killer = {
+    "name": "Miss Diane Brass",
+
+    "occupation": "Waitress",
+
+    "age":21,
+
+    "gender": "female",
+
+    "check": "The Killer",
+
+    "dialogue": []
+}
+denzel = {
+    "name": "Mr Denzel Brass",
+
+    "occupation": "Sport teacher",
+
+    "age": 60,
+
+    "gender": "male",
+
+    "check": "Committed suicide after the final verdict came back guilty."
+
+}
+barry = {
+    "name": "Barry Sloane",
+
+    "gender": "male",
+
+    "check": "Murdered by Mr Denzel Brass"
+}
+smith = {
+    "name": "Mr Smith",
+
+    "occupation": "Con artist",
+
+    "age": 27,
+
+    "gender": "male",
+
+    "check": "Mr Steven's Best Friend.",
+
+    "dialogue": []
+}
+blade = {
+    "name": "Blade",
+
+    "occupation": "Unemployed",
+
+    "age": 30,
+
+    "gender": "male",
+
+    "check": "Used to be Mr Steven Cornwall's cellmate.",
+
+    "dialogue": []
+}
+patrick = {
+    "name": "Mr Patrick",
+
+    "occupation": "Bank Manager",
+
+    "age": 47,
+
+    "gender": "male",
+
+    "check": "Brother-in-law to Mr Steven Cornwall.",
+
+    "dialogue": []
+}
+diane = {
+    "name": "Miss Diane B",
+
+    "occupation": "Waitress",
+
+    "age": 21,
+
+    "gender": "female",
+
+    "check": "Mistress to Mr Clark Davidson.",
+
+    "dialogue": []
+}
+james = {
+    "name": "Mr James Robin",
+
+    "occupation": "Business Man",
+
+    "age": 35,
+
+    "gender": "male",
+
+    "check": "Business partner to Mr Clark Davidson since 2013",
+
+    "dialogue": []
+}
+clark_2 = {
+    "name": "Mrs Clark Davidson",
+
+    "occupation": "Wife",
+
+    "age": 30,
+
+    "gender": "female",
+
+    "check": "Filed for divorce",
+
+    "dialogue": []
+}
+thomas = {
+    "name": "Thomas Reese",
+
+    "occupation": "Store Manager",
+
+    "age": 26,
+
+    "gender": "male",
+
+    "check": "Witness of a 6 year murder investigation.",
+
+    "dialogue": []
+}
+trisha = {
+    "name": "Trisha Banns",
+
+    "occupation": "Nurse",
+
+    "age": 30,
+
+    "gender": "female",
+
+    "check": "Witness of a 6 year murder investigation.",
+
+    "dialogue": []
+
+}
+christine = {
+    "name": "Miss Christine Lambart",
+
+    "occupation": "Stripper",
+
+    "age": 28,
+
+    "gender": "female",
+
+    "check": "Possession of illegal substance and a witness in a 6 year murder investigation.",
+
+    "dialogue": []
+}
+steven = {
+    "name": "Mr Steven Cornwall",
+
+    "occupation": "Civil servant",
+
+    "age": 38,
+
+    "gender": "male",
+
+    "check": "Theft, possession of illegal substance and witness in a 6 year murder investigation.",
+ 
+    "dialogue": []
+}
 kirill = {
     
     "name": "Kirill",
@@ -14,23 +177,23 @@ kirill = {
     
 }
 clark = {
-    "name" : "Clark",
+    "name" : "Mr Clark Davidson",
 
     "occupation": "Plumber",
 
-    "age": 47,
+    "age": 33,
 
-    "gender": "female",
+    "gender": "male",
 
-    "check": "none",
+    "check": "Witness in a 6 year murder investigation.",
 
     "dialogue" : ["Please leave me alone!"],
 
 }
 police_man = {
-    "name" : "PC Bobby",
-    "occupation" : "Copper",
-    "age" : 12,
+    "name" : "Detective Carter",
+    "occupation" : "Homicide Detective",
+    "age" : 36,
     "gender" : "male",
     "check" : "none",
     "dialogue" : ["Time to catch a killer!"]
@@ -39,7 +202,7 @@ police_man = {
 scene_1_police_officer = {
     "name" : "Police Officer",
     "occupation" : "Copper",
-    "age" : 12,
+    "age" : 31,,
     "gender" : "male",
     "check": "none",
     "dialogue": ["Horrible situation isn't it. The investigative team found an unfired 9mm bullet outside.",
@@ -51,4 +214,4 @@ getCharacter = {
     "Clark Davidson": clark, 
 }
 
-char_list = [kirill,clark,police_man,scene_1_police_officer]
+char_list = [kirill,clark,police_man,scene_1_police_officer,denzel,barry,smith,blade,patrick,diane,james,clark_2,diane_killer,thomas,trisha,christine,steven]
