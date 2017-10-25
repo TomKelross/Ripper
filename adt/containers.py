6f98ae94b7a988f9983c17c49a0a3066b36f6325
Tom <tkelross@gmail.com>
2017-10-25 17:00:09 +0100

    fixed formatting

--- adt/containers.py
+++ adt/containers.py
@@ -1,40 +1,34 @@
 from adt.items import *
 
 drawers = {
-    "name" : "Chest of drawers",
-    "description" : "Full of stuff",
-    "inventory" : [beer,beer,beer]
+    "name": "Chest of drawers",
+    "description": "Full of stuff",
+    "inventory": [beer, beer, beer]
 }
 
-<<<<<<< HEAD
 bin = {
-	"name" : "Bin in the XXX Video Store"
-	"description" : "While the rest of the store is trashed, this bin seems supiciously upright"
-	"inventory" : [knife]
+    "name": "Bin in the XXX Video Store",
+    "description": "While the rest of the store is trashed, this bin seems supiciously upright",
+    "inventory": [knife]
 }
 
 boot = {
-	"name" : "Car Boot"
-	"description" : "Car boot in which the body was found at the Docks"
-	"inventory" : [wrench]
+    "name": "Car Boot",
+    "description": "Car boot in which the body was found at the Docks",
+    "inventory": [wrench]
 }
 
 body_2 = {
-	"name" : "Body of Steven Cornwall"
-	"description" : "The Body of the late Steven Cornwall"
-	"inventory" : [phone]
+    "name": "Body of Steven Cornwall",
+    "description": "The Body of the late Steven Cornwall",
+    "inventory": [phone]
 }
 
-
-
-
-container_list = [drawers , bin , boot , body_2]
-=======
+container_list = [drawers, bin, boot, body_2]
 scene_1_bin = {
-    "name" : "Upright Bin",
-    "description" : "Suspiciously upright",
-    "inventory" : [murder_knife]
+    "name": "Upright Bin",
+    "description": "Suspiciously upright",
+    "inventory": [murder_knife]
 }
 
-container_list = [drawers,scene_1_bin]
->>>>>>> 80e0eb233e4e75303a473574e20c73112a15d41c
+container_list = [drawers, scene_1_bin]
